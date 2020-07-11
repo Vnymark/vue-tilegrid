41662df89dda50f665d1f2cb162b43d1c44f95e5
Viktor Nymark <vnymark@gmail.com>
2020-07-11 20:15:41 +0200

    Added tileGrid view and Tile component.

---
+++ src/components/Tile.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="square">
+        Test
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Tile",
+    methods: {
+        changeColor() {
+
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+
+    .square {
+        position: relative;
+        display: inline-block;
+        width: 20vh;
+        height: 19.5vh;
+        border: solid 1px red;
+        background: #2c3e50;
+    }
+    .body {
+        background: #2c3e50;
+    }
+</style>
---
+++ src/views/TileGrid.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <Tile v-for="index in 45" :key="index" v-bind:id="index"></Tile>
+    </div>
+</template>
+
+<script>
+    import Tile from "@/components/Tile.vue";
+
+    export default {
+        name: "TileGrid",
+        components: {
+            Tile,
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .squares{
+        display:inline-block;
+    }
+</style>
